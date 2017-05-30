33195ce92148368d05722e2ddef84d011089935b
abuhman <abuhman@users.noreply.github.com>
2017-05-30 09:59:14 -0500

    Update scancode_library_run_scan.py

--- scancode_library_run_scan.py
+++ scancode_library_run_scan.py
@@ -1,9 +1,9 @@
 import scancode
 from scancode import api
 
-print("Hello World")
+print('Hello World')
 ordered_dictionary = api.get_licenses('../scancode-toolkit-2.0.0.rc2/src/commoncode/command.py')
 for i in ordered_dictionary:
 	print(i)
 	print('Printing')
-print("Goodbye")
+print('Goodbye')
