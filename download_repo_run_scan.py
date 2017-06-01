ef07b523df1a6347b0eeda723a608724434d8c9e
abuhman <abuhman@users.noreply.github.com>
2017-06-01 08:11:22 -0500

    Update download_repo_run_scan.py

--- download_repo_run_scan.py
+++ download_repo_run_scan.py
@@ -1,25 +1,56 @@
 #This is an example of downloading a repo zip file, 
 #unzipping it and running a scan on it.
 #The example uses scancode, but dosocsv2 could also be used.
-#In the future, urls and paths will not be hardcoded.
+#Future updates:
+#1. Display help information if user didn't supply correct arguments
+#2. Handle if user did not give url to a zip file download
+#3. Save SPDX to document instead of stdout
+#4. Clearer methods (currently scan method also downloads and unzips as well as scans)
 
 import subprocess
 from subprocess import call
 import requests
-from os import remove
+import os
+from os import path, remove
 import shutil
+import zipfile
+import sys
 
-to_scan = requests.get('https://github.com/OSSHealth/ghdata/archive/master.zip', stream=True)
+def main():
+	scan(sys.argv[1])
 
-file_location = '/home/anna/Desktop/scancode/ghdata.zip'
+def scan(repo_zip_url):
+	#Getting a zip file to download
+	to_scan = requests.get(repo_zip_url, stream=True)
 
-with open(file_location, 'wb') as fd:
-	for chunk in to_scan.iter_content(chunk_size=1024):
-		fd.write(chunk)
+	#Get path to where zip file will be stored
+	#Name of the zip file
+	zip_file_name = repo_zip_url[repo_zip_url.rfind('/')+1:]
+	#Path to the directory containing the zip file
+	directory_path = os.path.dirname(os.path.realpath(__file__))
+	#Concatenate file name with path
+	file_location = directory_path + zip_file_name
 
-call(['unzip', file_location])
-remove(file_location)
+	#write zip file in chunks
+	with open(file_location, 'wb') as fd:
+		for chunk in to_scan.iter_content(chunk_size=1024):
+			fd.write(chunk)
 
-print subprocess.check_output(['scancode','/home/anna/Desktop/scancode/ghdata-master','--format','spdx-tv'])
+	#Unzip the zip file
+	zip_file = zipfile.ZipFile(file_location, 'r')
+	zip_file.extractall()
+	#Get the directory that results from unzipping
+	#(Assume there is only one)
+	extracted_directory = (zip_file.namelist())[0]
+	zip_file.close()
+	#Remove the zip file
+	remove(file_location)
 
-shutil.rmtree('/home/anna/Desktop/scancode/ghdata-master')
+	#Scan the unzipped directory
+	print subprocess.check_output(['scancode',extracted_directory,'--format','spdx-tv'])
+
+	#Remove the unzipped directory
+	shutil.rmtree(extracted_directory)
+
+
+if __name__ == "__main__": main()
