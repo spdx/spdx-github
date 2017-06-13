c09d2e83f4c3577e68584cf8b4f88607cb623960
abuhman <abuhman@users.noreply.github.com>
2017-06-13 07:32:51 -0500

    Use local directories for tests

--- run_scan/unit_test.py
+++ run_scan/unit_test.py
@@ -1,12 +1,12 @@
 import unittest
 import download_repo_run_scan
 from os import path, remove
-import shutil
+import shutil	
 
 #This tests a method that runs through the whole scanning process.
 #Since it calls a few other methods, it will fail also if those methods fail.
 class wholeScanTestCase(unittest.TestCase):
-	url = 'https://github.com/OSSHealth/ghdata/archive/master.zip'
+	url = 'https://github.com/abuhman/test_webhooks/archive/master.zip'
 	spdx_file_name = ''
 	def setUp(self):
 		self.spdx_file_name = download_repo_run_scan.download_repo_run_scan(self.url)
@@ -20,7 +20,7 @@ class wholeScanTestCase(unittest.TestCase):
 #of a local file at the returned location
 class downloadFileTestCase(unittest.TestCase):
 	file_location = ''
-	url = 'https://github.com/OSSHealth/ghdata/archive/master.zip'
+	url = 'https://github.com/abuhman/test_webhooks/archive/master.zip'
 	def setUp(self):
 		self.file_location = download_repo_run_scan.download_github_zip(self.url)
 
@@ -34,16 +34,12 @@ class downloadFileTestCase(unittest.TestCase):
 #For this I am using the same download method as above to get a zip file
 #from GitHub, so both tests will fail if the download method fails
 class unzipFileTestCase(unittest.TestCase):
-	file_location = ''
-	url = 'https://github.com/OSSHealth/ghdata/archive/master.zip'
+	file_location = 'test.zip'
 	extracted_directory = ''
 	def setUp(self):
-		self.file_location = download_repo_run_scan.download_github_zip(self.url)
 		self.extracted_directory = download_repo_run_scan.unzip_file(self.file_location)
 	
 	def tearDown(self):
-		#Remove the zip file
-		remove(self.file_location)
 		#Remove the unzipped directory
 		shutil.rmtree(self.extracted_directory)
 	
@@ -53,24 +49,15 @@ class unzipFileTestCase(unittest.TestCase):
 #This tests whether a file output is produced from calling the scan method.
 #It needs the download zip and extract zip methods to be working.
 class scanTestCase(unittest.TestCase):
-	file_location = ''
-	url = 'https://github.com/OSSHealth/ghdata/archive/master.zip'
-	extracted_directory = ''
+	directory = 'test2/'
 	spdx_file_name = ''
 	def setUp(self):
-		self.file_location = download_repo_run_scan.download_github_zip(self.url)
-		self.extracted_directory = download_repo_run_scan.unzip_file(self.file_location)
 		#Set output file name to the directory name .SPDX
-		self.spdx_file_name = self.extracted_directory[:-1] + '.SPDX'
+		self.spdx_file_name = self.directory[:-1] + '.SPDX'
 
 		#scan the extracted directory and put results in a named file
-		download_repo_run_scan.scan(self.extracted_directory, self.spdx_file_name)
-	
+		download_repo_run_scan.scan(self.directory, self.spdx_file_name, 'scancode', 'tag-value')
 	def tearDown(self):
-		#Remove the zip file
-		remove(self.file_location)
-		#Remove the unzipped directory
-		shutil.rmtree(self.extracted_directory)	
 		#Remove the scan results file
 		remove(self.spdx_file_name)
 
