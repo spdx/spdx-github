e273c6a739032e78d5137f679af9bf710686c13a
Anna Buhman <buhmana@gmail.com>
2017-06-15 10:12:53 -0500

    Move files to new folders

--- dosocs_run_scan.py
+++
@@ -1,2 +0,0 @@
-import subprocess
-print subprocess.check_output(['dosocs2','oneshot','DoSOCSv2-0.16.1.tar.gz'])
--- scancode_run_scan.py
+++
@@ -1,2 +0,0 @@
-import subprocess
-print subprocess.check_output(['scancode','scancode-toolkit-2.0.0.rc2/src/commoncode','--format','spdx-tv'])
