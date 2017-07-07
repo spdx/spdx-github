a22e02babdfa4da13a371656c30dc97a20c104a1
Anna Buhman <buhmana@gmail.com>
2017-07-07 11:59:30 -0500

    Updates to example code

--- example_code/dosocs_run_scan.py
+++ example_code/dosocs_run_scan.py
@@ -1,3 +1,3 @@
 import subprocess
 
-print subprocess.check_output(['dosocs2','oneshot','DoSOCSv2-0.16.1.tar.gz'])
+print subprocess.check_output(['dosocs2','oneshot','test.zip'])
--- example_code/github_authentication.py
+++ example_code/github_authentication.py
@@ -1,7 +1,3 @@
 import subprocess
 
-#This curl command from the command line works:
-#curl --user "abuhman" https://api.github.com/users/abuhman
-
-#This subprocess call to the same curl command returns "Bad credentials"
-print subprocess.check_output(['curl', '--user', '"abuhman"', 'https://api.github.com/users/abuhman'])
+print subprocess.check_output(['curl', '--user', 'abuhman', 'https://api.github.com/users/abuhman'])
