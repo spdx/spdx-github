17c8b0bb447b9bb68a48fbdf83efbcc83b91ae4b
Anna Buhman <buhmana@gmail.com>
2017-08-02 13:36:19 -0500

    Add Apache License to API server and client files

--- src/web_api_server.py
+++ src/web_api_server.py
@@ -1,4 +1,19 @@
 #!flask/bin/python
+
+# Copyright (c) Anna Buhman.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 from flask import Flask, request, jsonify
 import json
 from spdx_github import repo_scan
